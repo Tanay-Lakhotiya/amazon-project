d1e5744a35a5ee4810485a528e947bcc217407f9
Tanay-Lakhotiya <tanayr.lakhotiya@gmail.com>
2025-03-23 23:31:06 +0530

    Make product quantity selector interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -19,7 +19,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -63,12 +63,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       }
     });
 
+    const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
     if (matchingItem) {
-      matchingItem.quantity++;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
         productId,
-        quantity: 1
+        quantity
       });
     }
 
