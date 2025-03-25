be320e1caf19b0060db15b25a9d70cedc4e2b590
Tanay-Lakhotiya <tanayr.lakhotiya@gmail.com>
2025-03-25 21:07:31 +0530

    Update items in header when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -103,12 +103,18 @@ document.querySelectorAll('.js-delete-link').forEach((deleteLink) => {
 
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
+
+    updateCartQuantity();
   });
 });
 
-let cartQuantity = 0;
-cart.forEach((cartItem) => {
-  cartQuantity += cartItem.quantity;
-});
+updateCartQuantity();
 
-document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  
+  document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`; 
+}
