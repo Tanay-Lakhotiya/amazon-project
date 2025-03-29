b29d46f9d9d38066f74b32a79719c579a167dc1e
Tanay-Lakhotiya <tanayr.lakhotiya@gmail.com>
2025-03-29 19:43:32 +0530

    Load products from backend

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -6,8 +6,6 @@ import { renderPaymentSummary } from "./paymentSummary.js";
 import { renderCheckoutHeader } from "./checkoutHeader.js";
 //import { dayjs } from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
-renderOrderSummary();
-
 export function renderOrderSummary() {
 
   let cartSummaryHTML = '';
